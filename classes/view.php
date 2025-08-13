f7710f21810d5e6618379b4456a5c19dedfe0ae8
algoritmika <info@algoritmika.com>
2025-08-14 00:17:53 +0300

    Cleanup

--- classes/view.php
+++ classes/view.php
@@ -1,18 +1,32 @@
 <?php
+/**
+ * WBW Currency Switcher for WooCommerce - viewWcu Class
+ *
+ * @author  woobewoo
+ */
+
+defined( 'ABSPATH' ) || exit;
+
 abstract class viewWcu extends baseObjectWcu {
+
 	/**
 	 * @deprecated
 	 */
 	protected $_tpl = WCU_DEFAULT;
+
 	/**
 	 * @var string name of theme to load from templates, if empty - default values will be used
 	 */
 	protected $_theme = '';
+
 	/**
 	 * @var string module code for this view
 	 */
 	protected $_code = '';
 
+	/**
+	 * display.
+	 */
 	public function display($tpl = '') {
 		$tpl = (empty($tpl)) ? $this->_tpl : $tpl;
 
@@ -20,6 +34,10 @@ abstract class viewWcu extends baseObjectWcu {
 			echo $content;
 		}
 	}
+
+	/**
+	 * getPath.
+	 */
 	public function getPath($tpl) {
 		$path = '';
 		$parentModule = frameWcu::_()->getModule( $this->_code );
@@ -28,12 +46,24 @@ abstract class viewWcu extends baseObjectWcu {
 		}
 		return $path;
 	}
+
+	/**
+	 * getModule.
+	 */
 	public function getModule() {
 		return frameWcu::_()->getModule( $this->_code );
 	}
+
+	/**
+	 * getModel.
+	 */
 	public function getModel($code = '') {
 		return frameWcu::_()->getModule( $this->_code )->getController()->getModel($code);
 	}
+
+	/**
+	 * getContent.
+	 */
 	public function getContent($tpl = '') {
 		$tpl = (empty($tpl)) ? $this->_tpl : $tpl;
 		$path = $this->getPath($tpl);
@@ -47,27 +77,59 @@ abstract class viewWcu extends baseObjectWcu {
 		}
 		return false;
 	}
+
+	/**
+	 * setTheme.
+	 */
 	public function setTheme($theme) {
 		$this->_theme = $theme;
 	}
+
+	/**
+	 * getTheme.
+	 */
 	public function getTheme() {
 		return $this->_theme;
 	}
+
+	/**
+	 * setTpl.
+	 */
 	public function setTpl($tpl) {
 		$this->_tpl = $tpl;
 	}
+
+	/**
+	 * getTpl.
+	 */
 	public function getTpl() {
 		return $this->_tpl;
 	}
+
+	/**
+	 * init.
+	 */
 	public function init() {
 
 	}
+
+	/**
+	 * assign.
+	 */
 	public function assign($name, $value) {
 		$this->$name = $value;
 	}
+
+	/**
+	 * setCode.
+	 */
 	public function setCode($code) {
 		$this->_code = $code;
 	}
+
+	/**
+	 * getCode.
+	 */
 	public function getCode() {
 		return $this->_code;
 	}
@@ -85,12 +147,21 @@ abstract class viewWcu extends baseObjectWcu {
 		}
 		self::display($formTpl);
 	}
+
+	/**
+	 * sizeToPxPt.
+	 */
 	public function sizeToPxPt($size) {
 		if(!strpos($size, 'px') && !strpos($size, '%'))
 			$size .= 'px';
 		return $size;
 	}
+
+	/**
+	 * getInlineContent.
+	 */
 	public function getInlineContent($tpl = '') {
 		return preg_replace('/\s+/', ' ', $this->getContent($tpl));
 	}
+
 }
