636a2a73c2385422cef20e0835e73652551a0745
algoritmika <info@algoritmika.com>
2025-08-14 00:21:10 +0300

    "Creation of dynamic property is deprecated" notice fixed

--- modules/admin_nav/views/admin_nav.php
+++ modules/admin_nav/views/admin_nav.php
@@ -1,7 +1,30 @@
 <?php
+/**
+ * WBW Currency Switcher for WooCommerce - admin_navViewWcu Class
+ *
+ * @version 2.2.0
+ *
+ * @author  woobewoo
+ */
+
+defined( 'ABSPATH' ) || exit;
+
 class admin_navViewWcu extends viewWcu {
+
+	/**
+	 * breadcrumbsList.
+	 *
+	 * @version 2.2.0
+	 * @since   2.2.0
+	 */
+	public $breadcrumbsList;
+
+	/**
+	 * getBreadcrumbs.
+	 */
 	public function getBreadcrumbs() {
 		$this->assign('breadcrumbsList', dispatcherWcu::applyFilters('mainBreadcrumbs', $this->getModule()->getBreadcrumbsList()));
 		return parent::getContent('adminNavBreadcrumbs');
 	}
+
 }
