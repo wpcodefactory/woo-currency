43123807853d9ec20a2e83867a8484a3a28577ef
algoritmika <info@algoritmika.com>
2025-08-14 00:14:56 +0300

    Cleanup

--- classes/view.php
+++ classes/view.php
@@ -1,31 +1,31 @@
 <?php
 abstract class viewWcu extends baseObjectWcu {
-    /* 
-     * @deprecated
-     */
-    protected $_tpl = WCU_DEFAULT;
-    /*
-     * @var string name of theme to load from templates, if empty - default values will be used
-     */
-    protected $_theme = '';
-    /*
-     * @var string module code for this view
-     */
-    protected $_code = '';
+	/*
+	 * @deprecated
+	 */
+	protected $_tpl = WCU_DEFAULT;
+	/*
+	 * @var string name of theme to load from templates, if empty - default values will be used
+	 */
+	protected $_theme = '';
+	/*
+	 * @var string module code for this view
+	 */
+	protected $_code = '';
 
-    public function display($tpl = '') {
-        $tpl = (empty($tpl)) ? $this->_tpl : $tpl;
+	public function display($tpl = '') {
+		$tpl = (empty($tpl)) ? $this->_tpl : $tpl;
 
-        if(($content = $this->getContent($tpl)) !== false) {
-            echo $content;
-        }
-    }
+		if(($content = $this->getContent($tpl)) !== false) {
+			echo $content;
+		}
+	}
 	public function getPath($tpl) {
 		$path = '';
 		$parentModule = frameWcu::_()->getModule( $this->_code );
 		if(file_exists($parentModule->getModDir(). 'views'. DS. 'tpl'. DS. $tpl. '.php')) { //Then try to find it in module directory
-            $path = $parentModule->getModDir(). DS. 'views'. DS. 'tpl'. DS. $tpl. '.php';
-        }
+			$path = $parentModule->getModDir(). DS. 'views'. DS. 'tpl'. DS. $tpl. '.php';
+		}
 		return $path;
 	}
 	public function getModule() {
@@ -34,50 +34,50 @@ abstract class viewWcu extends baseObjectWcu {
 	public function getModel($code = '') {
 		return frameWcu::_()->getModule( $this->_code )->getController()->getModel($code);
 	}
-    public function getContent($tpl = '') {
-        $tpl = (empty($tpl)) ? $this->_tpl : $tpl;
-        $path = $this->getPath($tpl);
-        if($path) {
-            $content = '';
-            ob_start();
-            require($path);
-            $content = ob_get_contents();
-            ob_end_clean();
-            return $content;
-        }
-        return false;
-    }
-    public function setTheme($theme) {
-        $this->_theme = $theme;
-    }
-    public function getTheme() {
-        return $this->_theme;
-    }
-    public function setTpl($tpl) {
-        $this->_tpl = $tpl;
-    }
-    public function getTpl() {
-        return $this->_tpl;
-    }
-    public function init() {
+	public function getContent($tpl = '') {
+		$tpl = (empty($tpl)) ? $this->_tpl : $tpl;
+		$path = $this->getPath($tpl);
+		if($path) {
+			$content = '';
+			ob_start();
+			require($path);
+			$content = ob_get_contents();
+			ob_end_clean();
+			return $content;
+		}
+		return false;
+	}
+	public function setTheme($theme) {
+		$this->_theme = $theme;
+	}
+	public function getTheme() {
+		return $this->_theme;
+	}
+	public function setTpl($tpl) {
+		$this->_tpl = $tpl;
+	}
+	public function getTpl() {
+		return $this->_tpl;
+	}
+	public function init() {
 
-    }
-    public function assign($name, $value) {
-        $this->$name = $value;
-    }
-    public function setCode($code) {
-        $this->_code = $code;
-    }
-    public function getCode() {
-        return $this->_code;
-    }
-	
+	}
+	public function assign($name, $value) {
+		$this->$name = $value;
+	}
+	public function setCode($code) {
+		$this->_code = $code;
+	}
+	public function getCode() {
+		return $this->_code;
+	}
+
 	/**
 	 * This will display form for our widgets
 	 */
 	public function displayWidgetForm($data = array(), $widget = array(), $formTpl = 'form') {
 		$this->assign('data', $data);
-        $this->assign('widget', $widget);
+		$this->assign('widget', $widget);
 		if(frameWcu::_()->isTplEditor()) {
 			if($this->getPath($formTpl. '_ext')) {
 				$formTpl .= '_ext';
