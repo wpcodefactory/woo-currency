304315cbafbd495e3a26e88ee4ad029d492af0dc
algoritmika <info@algoritmika.com>
2025-08-17 19:23:56 +0300

    v2.2.1

--- wcu.php
+++ wcu.php
@@ -2,11 +2,11 @@
 /**
  * Plugin Name: WBW Currency Switcher for WooCommerce
  * Description: WBW Currency Switcher for WooCommerce allows the customers switch products prices to any currencies. Get rates converted in the real time with dynamic currency switcher.
- * Version: 2.2.0
+ * Version: 2.2.1
  * Author: woobewoo
  * Author URI: https://woobewoo.com
  * WC requires at least: 3.4.0
- * WC tested up to: 10.0
+ * WC tested up to: 10.1
  * Text Domain: woo-currency
  * Domain Path: /languages
  **/
