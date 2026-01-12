aac7f32e3aa47a2005978494f2e2aff1237a9cc3
algoritmika <info@algoritmika.com>
2026-01-12 13:23:37 +0200

    Cleanup

--- classes/installer.php
+++ classes/installer.php
@@ -10,17 +10,20 @@
 defined( 'ABSPATH' ) || exit;
 
 class installerWcu {
+
 	static public $update_to_version_method = '';
+
 	static private $_firstTimeActivated = false;
+
 	static public function init( $isUpdate = false ) {
 		global $wpdb;
-		$wpPrefix = $wpdb->prefix; /* add to 0.0.3 Versiom */
+		$wpPrefix = $wpdb->prefix; /* add to 0.0.3 Version */
 		require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
 		$current_version = get_option($wpPrefix. WCU_DB_PREF. 'db_version', 0);
 		if(!$current_version)
 			self::$_firstTimeActivated = true;
 		/**
-		 * modules 
+		 * modules
 		 */
 		if (!dbWcu::exist("@__modules")) {
 			dbDelta(dbWcu::prepareQuery("CREATE TABLE IF NOT EXISTS `@__modules` (
@@ -47,7 +50,7 @@ class installerWcu {
 				(NULL, 'currency_widget',1,1,'Currency Widget');");
 		}
 		/**
-		 *  modules_type 
+		 *  modules_type
 		 */
 		if(!dbWcu::exist("@__modules_type")) {
 			dbDelta(dbWcu::prepareQuery("CREATE TABLE IF NOT EXISTS `@__modules_type` (
@@ -74,7 +77,7 @@ class installerWcu {
 			) DEFAULT CHARSET=utf8"));
 			dbWcu::query("INSERT INTO `@__usage_stat` (code, visits) VALUES ('installed', 1)");
 		}
-		
+
 		$wcuOrdersCurrencies = get_option(WCU_DB_PREF. 'orders_currencies');
 		if (!empty($wcuOrdersCurrencies)) {
 			$wcuOrdersCurrencies = json_decode($wcuOrdersCurrencies, true);
@@ -83,7 +86,6 @@ class installerWcu {
 				if ($order) {
 					$order->set_currency($wcuCurrency);
 				}
-				//update_post_meta($orderId, '_order_currency', $wcuCurrency);
 			}
 			delete_option(WCU_DB_PREF. 'orders_currencies');
 		}
@@ -92,10 +94,11 @@ class installerWcu {
 	static public function setUsed() {
 		update_option(WCU_DB_PREF. 'plug_was_used', 1);
 	}
+
 	static public function isUsed() {
-		//return true;	// No welcome page for now
 		return (int) get_option(WCU_DB_PREF. 'plug_was_used');
 	}
+
 	static public function delete() {
 		self::_checkSendStat('delete');
 		global $wpdb;
@@ -107,18 +110,14 @@ class installerWcu {
 		delete_option($wpPrefix. WCU_DB_PREF. 'db_installed');
 		delete_option(WCU_DB_PREF. 'orders_currencies');
 	}
+
 	static public function deactivate() {
 		self::_checkSendStat('deactivate');
 		self::_updateOrdersCurrency();
 	}
+
 	static private function _checkSendStat($statCode) {
-		/*if(class_exists('frameWcu')
-			&& frameWcu::_()->getModule('promo')
-			&& frameWcu::_()->getModule('options')
-		) {
-			frameWcu::_()->getModule('promo')->getModel()->saveUsageStat( $statCode );
-			frameWcu::_()->getModule('promo')->getModel()->checkAndSend( true );
-		}*/
+
 	}
 
 	/**
@@ -145,7 +144,7 @@ class installerWcu {
 				'type' => 'shop_order',
 				'return' => 'ids'
 			));
-			
+
 			if (!empty($orders)) {
 				foreach ($orders as $orderId) {
 					$order = wc_get_order($orderId);
@@ -155,11 +154,9 @@ class installerWcu {
 							$order->set_currency($wcuCurrency);
 						}
 					}
-					//$oldCurrencies[$orderId] = get_post_meta($orderId, '_order_currency', true);
-					//update_post_meta($orderId, '_order_currency', $wcuCurrency);
 				}
 			}
-			
+
 			if (!empty($oldCurrencies)) {
 				update_option(WCU_DB_PREF. 'orders_currencies', json_encode($oldCurrencies, JSON_UNESCAPED_UNICODE));
 			}
@@ -168,11 +165,12 @@ class installerWcu {
 
 	static public function update() {
 		global $wpdb;
-		$wpPrefix = $wpdb->prefix; /* add to 0.0.3 Versiom */
+		$wpPrefix = $wpdb->prefix; /* add to 0.0.3 Version */
 		$currentVersion = get_option($wpPrefix. WCU_DB_PREF. 'db_version', 0);
 		if(!$currentVersion || version_compare(WCU_VERSION, $currentVersion, '>')) {
 			self::init( true );
 			update_option($wpPrefix. WCU_DB_PREF. 'db_version', WCU_VERSION);
 		}
 	}
+
 }
